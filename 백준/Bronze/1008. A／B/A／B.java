bd6f9c3a5bd777065368126311d378ce1e8c6f59
Choisoom <167949377+csm0062@users.noreply.github.com>
2024-05-22 19:55:59 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		
+		double num = (double) A / B;
+		System.out.println(num);
+	}
+
+}
