24cce37e8aba198db4ae296e91a27d9653d67972
Choisoom <167949377+csm0062@users.noreply.github.com>
2024-05-22 20:42:57 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+		
+		long sum = A + B + C;
+		
+		System.out.println(sum);
+		
+	}
+
+	
+}
